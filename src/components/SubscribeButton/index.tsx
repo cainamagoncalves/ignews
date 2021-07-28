3be0e4b2b4807e3f1b7076992f5de758e5e01129
Cainã <moaraadrean@gmail.com>
2021-07-27 22:07:14 -0300

    Criando banco com Fauna e comunicação com Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,33 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+// getServerSideProps (SSR)
+// getServerStaticProps (SSG)
+// API routes
+
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  function handleSubscribe() {
+
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+
+  }
+
   return (
     <button 
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
