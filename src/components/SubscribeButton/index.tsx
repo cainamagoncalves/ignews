a0bb89cbf61a7faddde77fe6841e792c59888415
Cainã <moaraadrean@gmail.com>
2021-07-28 20:34:00 -0300

    Finalizando conexão com stripe e Fauna

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -13,14 +16,26 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
 
     if (!session) {
-      signIn('github')
+      signIn('github') // CASO USUÁRIO NÃO ESTEJA LOGADO
       return;
     }
 
+    try {
+      const response = await api.post('/subscribe')
 
+      const { sessionId } = response.data;
+
+      
+      const stripe = await getStripeJs() // PEGAR USUÁRIO LOGADO
+
+      await stripe.redirectToCheckout({ sessionId }) // REDIRECIONAR USUÁRIO
+      
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
